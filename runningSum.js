fc58c3c403ff1bb8581790ff844689c2c7730328
ajmalgithubm <ajmalm76774@gmail.com>
2023-09-22 18:17:44 +0530

    new problem solved

---
+++ runningSum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+
+/*  
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums. */
+var runningSum = function (nums) {
+    const n = nums.length
+    const res = new Array(n).fill(0);
+    let sum = 0;
+    for (let i = 0; i < n; i++) {
+        sum += nums[i];
+        res[i] = sum
+    }
+    return res
+};
