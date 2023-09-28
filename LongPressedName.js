29ba5e99532cf7d9e9d6c95dcc6e1c6a1fc863f7
ajmalgithubm <ajmalm76774@gmail.com>
2023-09-28 11:52:05 +0530

    new problem solved

---
+++ LongPressedName.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} name
+ * @param {string} typed
+ * @return {boolean}
+ */
+var isLongPressedName = function (name, typed) {
+    let i = 0;
+    let j = 0;
+    while (j < typed.length) {
+        if (i < name.length && name[i] === typed[j]) {
+            i++;
+            j++;
+        } else if (j === 0 || typed[j] !== typed[j - 1]) {
+            return false
+        } else {
+            j++;
+        }
+    }
+    return name.length === i
+};
+/* 
+Your friend is typing his name into a keyboard. Sometimes, when typing a character c, the key might get long pressed, and the character will be typed 1 or more times.
+
+You examine the typed characters of the keyboard. Return True if it is possible that it was your friends name, with some characters (possibly none) being long pressed.
+*/
