8115836f099da404d75fd9eef37bff2178996713
ajmalgithubm <ajmalm76774@gmail.com>
2023-09-29 22:46:11 +0530

    new problem solved

---
+++ countmatchesInTheTournement.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numberOfMatches = function (n) {
+    let matches = 0;
+    while (n >= 2) {
+        if (n % 2 === 1) {
+            matches += (n - 1) / 2;
+            n = (n - 1) / 2 + 1
+        } else {
+            matches += n / 2;
+            n = n / 2;
+        }
+    }
+    return matches
+};
+/* 
+You are given an integer n, the number of teams in a tournament that has strange rules:
+
+If the current number of teams is even, each team gets paired with another team. A total of n / 2 matches are played, and n / 2 teams advance to the next round.
+If the current number of teams is odd, one team randomly advances in the tournament, and the rest gets paired. A total of (n - 1) / 2 matches are played, and (n - 1) / 2 + 1 teams advance to the next round.
+Return the number of matches played in the tournament until a winner is decided.
+*/
