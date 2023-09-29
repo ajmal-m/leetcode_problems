f6a7655305c6e8bb65885a666339d4d7c1d80aa2
ajmalgithubm <ajmalm76774@gmail.com>
2023-09-29 21:55:54 +0530

    new problem solved

---
+++ RichestCustomerWealth.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[][]} accounts
+ * @return {number}
+ */
+var maximumWealth = function (accounts) {
+    let wealth = 0;
+    for (let i = 0; i < accounts.length; i++) {
+        let sum = 0;
+        for (let j = 0; j < accounts[i].length; j++) {
+            sum += accounts[i][j]
+        }
+        wealth = Math.max(wealth, sum)
+    }
+    return wealth
+};
+
+/**
+ You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.
+
+A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
+ */
