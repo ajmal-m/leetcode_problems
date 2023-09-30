c7a1bd0d565346e10e4c03c9c1987c41af81fae7
ajmalgithubm <ajmalm76774@gmail.com>
2023-09-30 14:40:32 +0530

    new problem solved

---
+++ numberofRectanglescamfoSquraes.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[][]} rectangles
+ * @return {number}
+ */
+var countGoodRectangles = function (rectangles) {
+    let count = 0;
+    let maxSide = 0;
+    for (let i = 0; i < rectangles.length; i++) {
+        let [len, width] = rectangles[i];
+        let side = Math.min(len, width);
+        if (side > maxSide) {
+            maxSide = side;
+            count = 1;
+        } else if (side === maxSide) {
+            count++
+        }
+    }
+    return count
+};
+/* 
+You are given an array rectangles where rectangles[i] = [li, wi] represents the ith rectangle of length li and width wi.
+
+You can cut the ith rectangle to form a square with a side length of k if both k <= li and k <= wi. For example, if you have a rectangle [4,6], you can cut it to get a square with a side length of at most 4.
+
+Let maxLen be the side length of the largest square you can obtain from any of the given rectangles.
+
+Return the number of rectangles that can make a square with a side length of maxLen.
+*/
