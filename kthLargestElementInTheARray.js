de1cecbb893a468359c843422edea34a06e624ba
ajmalgithubm <ajmalm76774@gmail.com>
2023-10-17 19:14:07 +0530

    new problem solved

---
+++ kthLargestElementInTheARray.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function (nums, k) {
+    nums = nums.sort((a, b) => (b - a))
+    return nums[k - 1]
+};
+/*
+Given an integer array nums and an integer k, return the kth largest element in the array.
+
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Can you solve it without sorting?
+
+ 
+*/
