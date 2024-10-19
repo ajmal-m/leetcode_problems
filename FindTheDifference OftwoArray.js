7dc0fa2e967a2363a37eb9fdc862672d77b8df33
ajmal5410347 <ajmal@airtory.com>
2024-10-19 21:59:56 +0530

    Divide array into two added

--- FindTheDifference OftwoArray.js
+++ FindTheDifference OftwoArray.js
@@ -36,3 +36,23 @@ var findDifference = function(nums1, nums2) {
 
     return [ [...new Set(res[0])], [ ...new Set(res[1])]]
 };
+
+
+//MATHOD@
+
+
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[][]}
+ */
+var findDifference = function(nums1, nums2) {
+    nums1 = [ ...new Set(nums1)];
+    nums2 = [ ...new Set(nums2)];
+    let common = nums1.filter((item) => (nums2.includes(item)) );
+    let res = [ nums1.filter((item) => (!common.includes(item))), nums2.filter((item) => (!common.includes(item))) ]
+
+    return res
+
+};
