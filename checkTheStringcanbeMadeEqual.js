8f93a4c004772760fb0c24128dc2512cdd67772b
ajmal5410347 <ajmal@airtory.com>
2024-10-27 14:42:27 +0530

    One problem solution added

---
+++ checkTheStringcanbeMadeEqual.js
@@ -0,0 +1,90 @@
+/**
+ * 
+ * 
+ * You are given two strings s1 and s2, both of length 4, consisting of lowercase English letters.
+
+You can apply the following operation on any of the two strings any number of times:
+
+Choose any two indices i and j such that j - i = 2, then swap the two characters at those indices in the string.
+Return true if you can make the strings s1 and s2 equal, and false otherwise.
+ * 
+ */
+
+
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var canBeEqual = function(s1, s2) {
+    let s1Arr = [];
+    let s2Arr = [];
+
+    if(s1 == s2){
+        return true
+    }
+
+    let swapedS1 = s1.split("");
+    let c = swapedS1[0];
+    swapedS1[0] = swapedS1[2];
+    swapedS1[2] = c;
+    swapedS1 = swapedS1.join("")
+    s1Arr.push(swapedS1)
+
+
+    swapedS1 = s1.split("");
+    c = swapedS1[1];
+    swapedS1[1] = swapedS1[3];
+    swapedS1[3] = c;
+    swapedS1 = swapedS1.join("")
+    s1Arr.push(swapedS1)
+
+
+    swapedS1 = s1.split("");
+    c = swapedS1[0];
+    swapedS1[0] = swapedS1[2];
+    swapedS1[2] = c;
+
+    c = swapedS1[1];
+    swapedS1[1] = swapedS1[3];
+    swapedS1[3] = c;
+    swapedS1 = swapedS1.join("")
+    s1Arr.push(swapedS1)
+
+
+
+    swapedS1 = s2.split("");
+    c = swapedS1[0];
+    swapedS1[0] = swapedS1[2];
+    swapedS1[2] = c;
+    swapedS1 = swapedS1.join("")
+    s2Arr.push(swapedS1)
+
+
+    swapedS1 = s2.split("");
+    c = swapedS1[1];
+    swapedS1[1] = swapedS1[3];
+    swapedS1[3] = c;
+    swapedS1 = swapedS1.join("")
+    s2Arr.push(swapedS1)
+
+
+    swapedS1 = s2.split("");
+    c = swapedS1[0];
+    swapedS1[0] = swapedS1[2];
+    swapedS1[2] = c;
+    
+
+    c = swapedS1[1];
+    swapedS1[1] = swapedS1[3];
+    swapedS1[3] = c;
+    swapedS1 = swapedS1.join("")
+    s2Arr.push(swapedS1);
+
+
+    if(s1Arr.includes(s2) || s2Arr.includes(s1)){
+        return true
+    }else{
+        return false
+    }
+};
