42eea5b17d16d6c434690f021351e792c599b495
ajmal5410347 <ajmal@airtory.com>
2024-11-17 18:47:53 +0530

    problem solved

---
+++ smallestRange.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var smallestRangeI = function(nums, k) {
+    let max = Math.max(...nums);
+    let min = Math.min(...nums);
+
+    let s = Math.max(0, max-min-2*k);
+    return s
+};
+
+/**
+ * 
+ * 
+ * You are given an integer array nums and an integer k.
+
+In one operation, you can choose any index i where 0 <= i < nums.length and change nums[i] to nums[i] + x where x is an integer from the range [-k, k]. You can apply this operation at most once for each index i.
+
+The score of nums is the difference between the maximum and minimum elements in nums.
+
+Return the minimum score of nums after applying the mentioned operation at most once for each index in it.
+ * 
+ */
