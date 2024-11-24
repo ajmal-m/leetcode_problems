04297a42435efab9d0f93f0394798c8ffb6a1ebb
ajmal5410347 <ajmal@airtory.com>
2024-11-24 18:24:07 +0530

    problem solved

---
+++ buyChocolate.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} prices
+ * @param {number} money
+ * @return {number}
+ */
+var buyChoco = function(prices, money) {
+    prices = prices.sort((a,b) => (a-b));
+    if( money-prices[0]-prices[1] >=0  ){
+      return money-prices[0]-prices[1];
+    }else{
+      return money;
+    }
+};
+
+/**You are given an integer array prices representing the prices of various chocolates in a store. You are also given a single integer money, which represents your initial amount of money.
+
+You must buy exactly two chocolates in such a way that you still have some non-negative leftover money. You would like to minimize the sum of the prices of the two chocolates you buy.
+
+Return the amount of money you will have leftover after buying the two chocolates. If there is no way for you to buy two chocolates without ending up in */
