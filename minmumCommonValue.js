e623ce73f1a707a016cb72420602d028454a50c2
ajmal5410347 <ajmal@airtory.com>
2024-11-24 17:36:12 +0530

    problem solved

--- minmumCommonValue.js
+++ minmumCommonValue.js
@@ -3,28 +3,27 @@
  * @param {number[]} nums2
  * @return {number}
  */
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
 var getCommon = function(nums1, nums2) {
-    let hash = {};
-    let common = [];
+    let pointer1 = 0;
+    let pointer2 = 0;
 
-    for(let num of nums1){
-        if(hash[num]){
-            hash[num]++;
+    while( pointer1 < nums1.length && pointer2 < nums2.length ){
+        if( nums1[pointer1] < nums2[pointer2]){
+            pointer1++;
+        }else if( nums1[pointer1] > nums2[pointer2]){
+            pointer2++;
         }else{
-            hash[num] = 1;
+            return nums1[pointer1];
         }
     }
 
-    for(let num of nums2){
-        if(hash[num]){
-            common.push(num);
-        }
-    }
-
-    common = common.sort((a,b) => (a-b));
-    return common?.length ? common[0] : -1;
+    return -1;
 };
-
 
 /**Given two integer arrays nums1 and nums2, sorted in non-decreasing order, return the minimum integer common to both arrays. If there is no common integer amongst nums1 and nums2, return -1.
 
