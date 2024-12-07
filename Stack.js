ca02179bc2f12d943aa56b770bcbe0820ef125f7
ajmal5410347 <ajmal@airtory.com>
2024-12-07 22:20:59 +0530

    problem slved

---
+++ Stack.js
@@ -0,0 +1,57 @@
+class Stack{
+    constructor(){
+        this.count = 0;
+        this.strorage = {};
+    }
+
+
+    push(value){
+        this.strorage[this.count] = value;
+        this.count++;
+    }
+
+    pop(){
+        if(this.count == 0){
+            return undefined
+        }
+        this.count--;
+        let res = this.strorage[this.count];
+        delete this.strorage[this.count];
+        return res;
+    }
+
+    size(){
+        return this.count;
+    }
+
+    peak(){
+        return this.strorage[this.count-1];
+    }
+
+    display(){
+        return this.strorage;
+    }
+}
+
+
+const stack = new Stack();
+
+stack.push(55);
+
+stack.push(89);
+
+// console.log(stack.peak());
+
+stack.push(78);
+
+stack.push(67);
+
+
+stack.push(39);
+
+
+
+
+// console.log(stack.peak());
+
+console.log(stack.display());
